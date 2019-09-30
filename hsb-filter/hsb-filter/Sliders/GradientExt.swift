44038e0cbe356c2fd4629b4ebe4e9fd8341a1504
David Arayan <david.arayan12@gmail.com>
2019-09-30 18:36:45 +1000

    - Added GradientExt.getRGBA and colorAt functionality. This will be used to change the color of the slider at a given position. - Added Hue/Saturation/Brightness 50

--- hsb-filter/hsb-filter/Sliders/GradientExt.swift
+++ hsb-filter/hsb-filter/Sliders/GradientExt.swift
@@ -31,7 +31,7 @@ extension UISlider {
     /**
      * Sets the background gradient provided a list of gradients and a height value for the slider
      */
-    public func setBackgroundGradient(gradients: [GradientValue], height:CGFloat) {
+    func setBackgroundGradient(gradients: [GradientValue], height:CGFloat) {
         let gradientLayer = gradients.gradientLayer
         
         gradientLayer.frame = CGRect(x: 0.0, y: 0.0, width: self.frame.size.width, height: height)
@@ -92,9 +92,77 @@ extension Array where Element == GradientValue {
             gradientLayer.colors = self.colorList
             gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
             gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
-            gradientLayer.cornerRadius = 1.25;
+            gradientLayer.cornerRadius = 1.25
             
             return gradientLayer
         }
+    }
+    
+    /**
+     * Helper function that returns the RGBA Colors as a tuple at the provided
+     * array position. This is used for performing interpolation operations on our
+     * list of gradient values.
+     */
+    func getRGBA(atIndex:Int) -> (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        
+        self[atIndex].color.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+
+        return (red, green, blue, alpha)
+    }
+    
+    /**
+     * Perform a linear interpolation function using the current gradient array, taking into
+     * account the positional information
+     */
+    func colorAt(position:Float) -> UIColor {
+        // WARNING: Float equality comparison
+        if (position <= self[0].position) {
+            return self[0].color
+        }
+        
+        // WARNING: Float equality comparison
+        if (position >= self[count - 1].position) {
+            return self[count - 1].color
+        }
+        
+        var end:Int = 1
+        
+        // linear search which is O(n). This is ok considering we
+        // are not dealing with super-massive arrays, however if
+        // performance is an issue, can switch to Binary Search
+        // which is O(log(n))
+        while (self[end].position < position) {
+            end += 1
+        }
+        
+        // this involves alot of array access, and can be optimised
+        // if array lookups (for whatever reason) are slow. Can be a performance
+        // penalty if doing millions of ops per frame, not a concern here
+        let spanTime:Float = (position - self[end - 1].position) / (self[end].position - self[end - 1].position)
+        
+        let colorA = self.getRGBA(atIndex: end - 1)
+        let colorB = self.getRGBA(atIndex: end)
+        
+        // define our lerp function
+        func lerp(a:CGFloat, b:CGFloat, t:CGFloat) -> CGFloat {
+            return (a * (1.0 - t)) + (b * t);
+        }
+        
+        // now we lerp(colorA, colorB, spanTime)
+        // return (a * (1.0f - f)) + (b * f);
+        let cgSpanTime:CGFloat = CGFloat(spanTime)
+        
+        // this could be SIMD accelerated for performance
+        let lerpR:CGFloat = lerp(a: colorA.red, b: colorB.red, t: cgSpanTime)
+        let lerpG:CGFloat = lerp(a: colorA.green, b: colorB.green, t: cgSpanTime)
+        let lerpB:CGFloat = lerp(a: colorA.blue, b: colorB.blue, t: cgSpanTime)
+        let lerpA:CGFloat = lerp(a: colorA.alpha, b: colorB.alpha, t: cgSpanTime)
+        
+        // create and return our generated/lerped UIColor
+        return UIColor(red: lerpR, green: lerpG, blue: lerpB, alpha: lerpA)
     }
 }
--- hsb-filter/hsb-filter/ViewController.swift
+++ hsb-filter/hsb-filter/ViewController.swift
@@ -18,31 +18,40 @@ class ViewController: UIViewController {
     }
     
     override func viewDidLayoutSubviews() {
-         let hueGradients:[GradientValue] = [
-             GradientValue(color: UIColor(named: "Hue 0")!, position: 0.0),
-             GradientValue(color: UIColor(named: "Hue 13")!, position: 0.13),
-             GradientValue(color: UIColor(named: "Hue 31")!, position: 0.31),
-             GradientValue(color: UIColor(named: "Hue 45")!, position: 0.45),
-             GradientValue(color: UIColor(named: "Hue 56")!, position: 0.56),
-             GradientValue(color: UIColor(named: "Hue 66")!, position: 0.66),
-             GradientValue(color: UIColor(named: "Hue 73")!, position: 0.73),
-             GradientValue(color: UIColor(named: "Hue 87")!, position: 0.87),
-             GradientValue(color: UIColor(named: "Hue 100")!, position: 1.0)
-         ]
+        
+        // NOTE: This is done for the purposes of simplicity. In a real implementation
+        // this would likely be its own construct/class to ensure positions are always mapped
+        // between 0.0 and 1.0. This is to protect other developers and our future selves from
+        // simple logic errors.
+        // See GradientExt that contains extension methods to deal with an array of GradientValue
+        let hueGradients:[GradientValue] = [
+            GradientValue(color: UIColor(named: "Hue 0")!, position: 0.0),
+            GradientValue(color: UIColor(named: "Hue 13")!, position: 0.13),
+            GradientValue(color: UIColor(named: "Hue 31")!, position: 0.31),
+            GradientValue(color: UIColor(named: "Hue 45")!, position: 0.45),
+            GradientValue(color: UIColor(named: "Hue 50")!, position: 0.50),
+            GradientValue(color: UIColor(named: "Hue 56")!, position: 0.56),
+            GradientValue(color: UIColor(named: "Hue 66")!, position: 0.66),
+            GradientValue(color: UIColor(named: "Hue 73")!, position: 0.73),
+            GradientValue(color: UIColor(named: "Hue 87")!, position: 0.87),
+            GradientValue(color: UIColor(named: "Hue 100")!, position: 1.0)
+        ]
          
-         let satGradients:[GradientValue] = [
-             GradientValue(color: UIColor(named: "Saturation 0")!, position: 0.0),
-             GradientValue(color: UIColor(named: "Saturation 100")!, position: 1.0)
-         ]
+        let satGradients:[GradientValue] = [
+            GradientValue(color: UIColor(named: "Saturation 0")!, position: 0.0),
+            GradientValue(color: UIColor(named: "Saturation 50")!, position: 0.50),
+            GradientValue(color: UIColor(named: "Saturation 100")!, position: 1.0)
+        ]
          
-         let briGradients:[GradientValue] = [
-             GradientValue(color: UIColor(named: "Brightness 0")!, position: 0.0),
-             GradientValue(color: UIColor(named: "Brightness 100")!, position: 1.0)
-         ]
+        let briGradients:[GradientValue] = [
+            GradientValue(color: UIColor(named: "Brightness 0")!, position: 0.0),
+            GradientValue(color: UIColor(named: "Brightness 50")!, position: 0.50),
+            GradientValue(color: UIColor(named: "Brightness 100")!, position: 1.0)
+        ]
          
-         hueSlider.setBackgroundGradient(gradients: hueGradients, height: 2.0)
-         saturationSlider.setBackgroundGradient(gradients: satGradients, height: 2.0)
-         brightnessSlider.setBackgroundGradient(gradients: briGradients, height: 2.0)
+        hueSlider.setBackgroundGradient(gradients: hueGradients, height: 2.0)
+        saturationSlider.setBackgroundGradient(gradients: satGradients, height: 2.0)
+        brightnessSlider.setBackgroundGradient(gradients: briGradients, height: 2.0)
     }
 }
 
